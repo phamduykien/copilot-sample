198792b64244038168a494c5817b348b9e456799
phamduykien <phamduykien@gmail.com>
2024-06-17 11:10:29 +0700

    update

---
+++ sql/employee.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE Employee
+(
+    Name NVARCHAR(50),
+    Department NVARCHAR(50),
+    Salary INT,
+    Age INT,
+    Address NVARCHAR(50),
+    PhoneNumber NVARCHAR(50)
+)
---
+++ sql/optimize-cursor.sql
@@ -0,0 +1,43 @@
+-- CURSOR
+DECLARE @StockItemID INT, @Description NVARCHAR(50)
+
+DECLARE @SalesDetail TABLE
+(
+	OrderID INT NOT NULL,
+	OrderLineID INT,
+	Quantity int,
+	PickedQuantity INT,
+	StockItemID INT NOT NULL,
+	Description NVARCHAR(50) NOT NULL
+)
+DECLARE MyCursorName --create cursor
+CURSOR FAST_FORWARD
+FOR
+	SELECT StockItemID, BinLocation
+--cursor base SELECT statement
+FROM Warehouse.StockItemHoldings
+WHERE BinLocation LIKE 'K-9'
+
+OPEN MyCursorName
+FETCH NEXT FROM MyCursorName
+INTO @StockItemID, @Description
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	INSERT INTO @SalesDetail
+		(OrderID, StockItemID, Description, OrderLineID, Quantity, PickedQuantity)
+	SELECT OrderID, @StockItemID, @Description, OrderLineID, Quantity, PickedQuantity
+	FROM Sales.OrderLines
+	WHERE StockItemID = @StockItemID
+
+	FETCH NEXT FROM MyCursorName
+INTO @StockItemID, @Description
+
+END
+CLOSE MyCursorName
+DEALLOCATE MyCursorName
+
+SELECT OrderLineID, Description, SUM(Quantity) AS Quantity, SUM(PickedQuantity) AS PickedQuantityX
+FROM @SalesDetail sd
+	JOIN Sales.Orders h ON sd.OrderID = h.OrderID
+GROUP BY OrderLineID, Description
